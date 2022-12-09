0e10c066fb3425a6ff2940feb968c673942de271
Awwalajide <ajide.awwal@gmail.com>
2022-12-09 11:39:22 -0800

    100-print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,9 @@ int main(void)
 	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
-		{	
+		{
 			if (m > n)
-			{	
+			{
 				putchar(n);
 				putchar(m);
 				if (n !=56 || m !=57)
@@ -26,7 +26,7 @@ int main(void)
 					putchar(' ');
 				}	
 			}
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
