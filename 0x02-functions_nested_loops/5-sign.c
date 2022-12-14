9b1d0c0c0d348e9daa19fc3f4f9091ae499de46e
Awwalajide <ajide.awwal@gmail.com>
2022-12-14 04:59:21 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,13 +16,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar('48');
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar('_');
-		return (-1);
 	}
-
+		return (-1);
 }
